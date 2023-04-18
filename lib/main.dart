381f10a483493be16583d2d1b9507591a6b7716b
HectorAgHe <aguilarhector440@gmail.com>
2023-04-17 23:08:53 -0600

    3

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:url_strategy/url_strategy.dart';
+
+import 'src/my_web_page.dart';
+//import 'package:riego_inteligente_web/src/my_page_web.dart';
+//import 'package:url_strategy/url_strategy.dart'
+
+void main() {
+  setPathUrlStrategy();
+  runApp(ProviderScope(
+      child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Sistema de Riego Inteligente',
+    home: MyWebPage(),
+  )));
+  // initialRoute:
+  // '/';
+  // routes:
+  // <String, WidgetBuilder>{
+  //   '/': (BuildContext context) => MyWebPage(),
+  //   'Login': (BuildContext context) => LoginScreen(),
+  // };
+}
+
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//             // child: MyWebPage()
+//             ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/src/my_web_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'content/contact_content .dart';
+import 'content/features_content.dart';
+import 'content/home_content.dart';
+import 'content/screenshots_content.dart';
+import 'navigation_bar/nav_bar.dart';
+
+final homeKey = new GlobalKey();
+final featureKey = new GlobalKey();
+final screenshotKey = new GlobalKey();
+final contactKey = new GlobalKey();
+//final loginKey = new GlobalKey();
+
+final currentPageProvider = StateProvider<GlobalKey>((_) => homeKey);
+final scrolledProvider = StateProvider<bool>((_) => false);
+
+class MyWebPage extends HookConsumerWidget {
+  void onScroll(ScrollController controller, WidgetRef ref) {
+    final isScrolled = ref.read(scrolledProvider);
+
+    if (controller.position.pixels > 5 && !isScrolled) {
+      ref.read(scrolledProvider.state).state = true;
+    } else if (controller.position.pixels <= 5 && isScrolled) {
+      ref.read(scrolledProvider.state).state = false;
+    }
+  }
+
+  void scrollTo(GlobalKey key) => Scrollable.ensureVisible(key.currentContext!,
+      duration: Duration(milliseconds: 500));
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _controller = useScrollController();
+
+    // How to set a listener: https://stackoverflow.com/a/63832263/3479489
+    useEffect(() {
+      _controller.addListener(() => onScroll(_controller, ref));
+      return _controller.dispose;
+    }, [_controller]);
+
+    double width = MediaQuery.of(context).size.width;
+    double maxWith = width > 1200 ? 1200 : width;
+
+    ref
+        .watch(currentPageProvider.state)
+        .addListener(scrollTo, fireImmediately: false);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: maxWith,
+          child: Column(
+            children: [
+              NavBar(),
+              Expanded(
+                child: SingleChildScrollView(
+                  controller: _controller,
+                  child: Column(
+                    children: <Widget>[
+                      HomeContent(key: homeKey),
+                      FeaturesContent(key: featureKey),
+                      ScreenshotsContent(key: screenshotKey),
+                      ContactContent(key: contactKey),
+                      //LoginScreen(key: loginKey,),
+                      SizedBox(height: 50),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
